a508d4fa35a5438eeb541637d72305b927e5123f
Gygain <58959684+Gygain@users.noreply.github.com>
2022-11-05 19:49:27 -0400

    Added files

---
+++ src/components/MyGitHub.jsx
@@ -0,0 +1,8 @@
+const MyGitHub = () => {
+    return (
+        <p>
+            Link to GitHub: <a href="https://github.com/"> https://github.com/</a>
+        </p>
+    )
+}
+export default MyGitHub;
